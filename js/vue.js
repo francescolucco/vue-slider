0f6d564276b71f3aadb9d459baf6c6f4ab57c883
francescolucco <francescolucco@googlemail.com>
2021-11-11 17:09:20 +0100

    first commit

---
+++ js/vue.js
@@ -0,0 +1,52 @@
+const app = new Vue({
+
+  el: '#app',
+
+  data: {
+    images: [
+      'img/01.jpg',
+      'img/02.jpg',
+      'img/03.jpg',
+      'img/04.jpg',
+      'img/05.jpg'
+    ],
+    titles: [
+      'Svezia',
+      'Svizzera',
+      'Gran Bretagna',
+      'Germania',
+      'Paradise'
+    ],
+    texts: [
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+      'Lorem ipsum',
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+      'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+    ],
+    counter: 0
+  },
+
+  mounted(){
+    console.log('app montata');
+  },
+
+  methods: {
+
+    nextSlide(){
+      this.counter--;
+      if(this.counter < 0){
+        this.counter = this.images.length - 1;
+      }
+
+    },
+
+    prevSlide(){
+      this.counter++;
+      if(this.counter > this.images.length - 1){
+        this.counter = 0;
+      }
+    }
+  }
+
+});
